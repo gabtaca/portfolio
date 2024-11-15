b6ac37e30d194db28612eeae60ff9c256a3ab590
Gabtaca <gabrieltaca117@gmail.com>
2024-11-15 09:20:18 -0500

    ajustement responsive height

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,24 @@
 // src/pages/_app.js
 
+import { useEffect } from 'react';
 import '../styles/styles.scss';
 import 'animate.css';
 import Layout from '../components/Layout';
 
-
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const resizeOps = () => {
+      document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+    };
+
+    // Run initially and on resize
+    resizeOps();
+    window.addEventListener("resize", resizeOps);
+
+    // Cleanup on unmount
+    return () => window.removeEventListener("resize", resizeOps);
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
